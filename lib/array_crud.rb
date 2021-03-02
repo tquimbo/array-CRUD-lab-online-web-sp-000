4fcbd67de4c44e743c1e95016ef984d6845bf906
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 13:32:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,10 +33,10 @@ end
 
 def retrieve_first_element_from_array(array)
   stuff = ["wow", "stuff", "more", "arrays"]
-  array[3]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array = []
-  array.unshift
+  stuff = ["wow", "stuff", "more", "arrays"]
+  array[0]
 end
