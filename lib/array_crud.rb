b196ecab49c0dcb0d0e9d0ce63966d786284be9b
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 13:33:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  stuff = ["wow", "stuff", "more", "arrays"]
+  stuff = ["wow", "stuff", "more", "arrays!"]
   array[3]
 end
