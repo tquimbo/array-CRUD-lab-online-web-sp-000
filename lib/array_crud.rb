00874afb08393621a5992873acc8b237f5b1de1b
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 13:28:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   stuff = ["what", "stuff", "more", "last"]
-  remove = stuff.unshift
+  remove = stuff.shift
 end
 
 def retrieve_element_from_index(array, index_number)
