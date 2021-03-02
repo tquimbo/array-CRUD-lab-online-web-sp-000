82ef6c7551b2167f77bfdecc6a25a459128ee05b
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 13:22:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,40 +3,40 @@ empty = []
 end
 
 def create_an_array
-stuff = ["what", "stuff"]  
+stuff = ["what", "stuff"]
 end
 
 def add_element_to_end_of_array(array, element)
 array = []
-array.push('element')  
+array.push('element')
 end
 
 def add_element_to_start_of_array(array, element)
 array = []
-array.shift(element)  
+array.shift(element)
 end
 
 def remove_element_from_end_of_array(array)
   array = []
-  array.unshift   
+  array.unshift
 end
 
 def remove_element_from_start_of_array(array)
   array = []
-  array.unshift   
+  array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
   array = []
-  array.unshift 
+  array.unshift
 end
 
 def retrieve_first_element_from_array(array)
   array = []
-  array.unshift 
+  array.unshift
 end
 
 def retrieve_last_element_from_array(array)
   array = []
-  array.unshift 
+  array.unshift
 end
